c92890ec996641a0e2762b44cddbedc1fb290c7b
nap3831 <nap3831@gmail.com>
2019-11-24 19:56:50 +0700

    Updates

---
+++ precache-manifest.bb0ec64e791bc81265ec69d62175b10e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b86621aab1313b6b41287352ba518e63",
+    "url": "/yoottana-nap/index.html"
+  },
+  {
+    "revision": "140cc7ee39c202cf1d6a",
+    "url": "/yoottana-nap/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "00e6c59468be9f113989",
+    "url": "/yoottana-nap/static/css/main.ee550eb1.chunk.css"
+  },
+  {
+    "revision": "140cc7ee39c202cf1d6a",
+    "url": "/yoottana-nap/static/js/2.30311682.chunk.js"
+  },
+  {
+    "revision": "00e6c59468be9f113989",
+    "url": "/yoottana-nap/static/js/main.6a7c3f63.chunk.js"
+  },
+  {
+    "revision": "43ce011740edd66a386a",
+    "url": "/yoottana-nap/static/js/runtime~main.b311d8e1.js"
+  }
+]);
